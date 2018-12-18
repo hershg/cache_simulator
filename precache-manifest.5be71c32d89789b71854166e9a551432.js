df1372b2e7059dfe9b6653925f6b32cfcc2b69d9
hershg <hersh.godse@gmail.com>
2018-12-18 00:35:09 -0800

    Updates

---
+++ precache-manifest.5be71c32d89789b71854166e9a551432.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae1fc3e534a49d3ff12f",
+    "url": "/static/js/main.ae1fc3e5.chunk.js"
+  },
+  {
+    "revision": "57d6b907e99a0179e82b",
+    "url": "/static/js/1.57d6b907.chunk.js"
+  },
+  {
+    "revision": "2b1ab6a4091cb127c56f05e9ca589356",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-tutorial/precache-manifest.baf9655f02a39f020af0474f0f254ec1.js"
+  "/precache-manifest.5be71c32d89789b71854166e9a551432.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-tutorial/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
