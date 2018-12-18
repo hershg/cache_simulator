bf58a54b5b83389451dba0194f469bff78e75b87
hershg <hersh.godse@gmail.com>
2018-12-18 00:19:45 -0800

    Updates

---
+++ precache-manifest.40665b66195f7c688638d0f9b373e0f4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "a956739e406755553cb3",
+    "url": "/react-tutorial/static/js/main.a956739e.chunk.js"
+  },
+  {
+    "revision": "57d6b907e99a0179e82b",
+    "url": "/react-tutorial/static/js/1.57d6b907.chunk.js"
+  },
+  {
+    "revision": "73d2d18428dd6a9742708d95c3c5100e",
+    "url": "/react-tutorial/index.html"
+  }
+];
